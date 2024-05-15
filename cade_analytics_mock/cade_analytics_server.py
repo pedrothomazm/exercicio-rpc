6e817a88bca4634a18e136a8b4c4dae6c8084309
Pedro Thomaz Conzatti Martins <pedrothomazm@gmail.com>
2024-05-15 20:25:39 -0300

    Crie estrutura do servidor

---
+++ cade_analytics_mock/cade_analytics_server.py
@@ -0,0 +1,33 @@
+from concurrent import futures
+
+import sys
+sys.path.append('generated')
+
+import grpc
+import event_receiver_pb2
+import event_receiver_pb2_grpc
+
+
+# Classe que implementa o serviço gRPC
+class EventReceiverServicer(event_receiver_pb2_grpc.EventReceiverServicer):
+    def SendEvents(self, request, context):
+        # TODO: Implementar a lógica de processamento dos eventos aqui
+        # Imprime para confirmar que os eventos foram recebidos
+        print("Received events: %s" % request)
+        return event_receiver_pb2.Empty()
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    event_receiver_pb2_grpc.add_EventReceiverServicer_to_server(
+        EventReceiverServicer(), server
+    )
+    server.add_insecure_port("[::]:50051")
+    server.start()
+    print("Server started")
+    server.wait_for_termination()
+
+
+if __name__ == "__main__":
+    print("Starting server")
+    serve()
