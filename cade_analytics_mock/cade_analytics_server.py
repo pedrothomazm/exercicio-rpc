c6c668fa1ad7b6318a4050db6a44c0941ae6093d
Pedro Thomaz Conzatti Martins <pedrothomazm@gmail.com>
2024-05-17 20:46:39 -0300

    Altere comportamento do servidor

--- cade_analytics_mock/cade_analytics_server.py
+++ cade_analytics_mock/cade_analytics_server.py
@@ -1,9 +1,11 @@
 from concurrent import futures
 from datetime import datetime
 import threading
+import time
 
 import sys
 import pandas as pd
+
 sys.path.append("generated")
 
 import grpc
@@ -13,15 +15,16 @@ import event_receiver_pb2_grpc
 Empty = event_receiver_pb2.google_dot_protobuf_dot_empty__pb2.Empty
 
 # Initialize a DataFrame to store events
-events_df = pd.DataFrame(columns = ["date", "user_id", "stimulus", "target"])
+events_df = pd.DataFrame(columns=["date", "user_id", "stimulus", "target"])
 df_lock = threading.Lock()
+
 
 # Classe que implementa o serviço gRPC
 class EventReceiverServicer(event_receiver_pb2_grpc.EventReceiverServicer):
     def SendEvents(self, request, context):
         """
         Receives a list of events and processes them.
-        
+
         Args:
             request: The EventList message containing one or more events.
             context: The gRPC context for the call.
@@ -31,40 +34,25 @@ class EventReceiverServicer(event_receiver_pb2_grpc.EventReceiverServicer):
         """
 
         global events_df
-        new_events  = []
+        new_events = []
 
         # Append new events to the DataFrame
         for event in request.events:
-            new_events.append({
-                "date": datetime.fromtimestamp(event.date),
-                "user_id": event.user_id,
-                "stimulus": event.stimulus,
-                "target": event.target
-            })
+            new_events.append(
+                {
+                    "date": datetime.fromtimestamp(event.date),
+                    "user_id": event.user_id,
+                    "stimulus": event_receiver_pb2.Stimulus.Name(event.stimulus),
+                    "target": event_receiver_pb2.Target.Name(event.target),
+                }
+            )
 
         # Convert list of dictionaries to DataFrame and concatenate with the existing DataFrame
         new_events_df = pd.DataFrame(new_events)
         with df_lock:
             events_df = pd.concat([events_df, new_events_df], ignore_index=True)
 
-        print("Received events: %s" % request)
-        
-        # Next, we perform some simple analyzes in order to, very briefly, test the following aspects of the code:
-            # - Make sure the server is receiving, processing and analyzing data correctly.
-            # - Measure the impact on server performance when performing these operations (processing time).
-            # - Evaluate how server response time varies depending on the load (number of events and complexity of analyses).
-
-        # count number of events per user
-        user_event_counts = events_df["user_id"].value_counts()
-        print("Number of events per user:\n", user_event_counts)
-
-        # Count events per target component
-        target_event_counts = events_df["target"].value_counts()
-        print("Number of events per target component:\n", target_event_counts)
-
-        # Count number of events per date
-        date_event_counts = events_df["date"].value_counts()
-        print("Number of events per date:\n", date_event_counts)
+        print(f"Received {len(request.events)} events")
 
         return Empty()
 
@@ -86,7 +74,7 @@ def serve():
     )
 
     # Bind the server to a port on all interfaces
-        ## listening on port 50051 on all available IPv4 and IPv6 addressess
+    ## listening on port 50051 on all available IPv4 and IPv6 addressess
     server.add_insecure_port("[::]:50051")
 
     # Non-blocking call => server runs in the background
@@ -100,8 +88,44 @@ def serve():
 
     print("Server stopped")
 
-    
-if __name__ == "__main__":
-    print("Starting server")
+
+def continuous_analysis(interval=10):
+    """
+    Periodically performs analysis on the events DataFrame.
+
+    Args:
+        interval (int): Interval in seconds between each analysis.
+    """
+
+    while True:
+        time.sleep(interval)
+
+        with df_lock:
+            copy = events_df.copy()
+
+        # count number of events per user
+        user_event_counts = copy["user_id"].value_counts()
+        print("\n\nNumber of events per user:\n", user_event_counts)
+
+        # Count events per target component
+        target_event_counts = copy["target"].value_counts()
+        print("\nNumber of events per target component:\n", target_event_counts)
+
+        # Count number of events per day
+        copy["date"] = pd.to_datetime(copy["date"]).dt.date
+        daily_event_counts = copy["date"].value_counts()
+        print("\nNumber of events per day:\n", daily_event_counts)
+
+
+def main():
+    # Start the continuous analysis thread as a daemon, so
+    # it stops when the main thread stops
+    analysis_thread = threading.Thread(target=continuous_analysis, daemon=True)
+    analysis_thread.start()
+
+    # Start the gRPC server
     serve()
-    
+
+
+if __name__ == "__main__":
+    main()
